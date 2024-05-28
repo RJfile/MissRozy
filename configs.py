e14706db88cfc2c07af34f879cdfa86068e9c553
RJfile <rkyadav512@gmail.com>
2024-05-28 12:49:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -14,8 +14,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN','lazy-gangster-baby-lazydeveloperr.koyeb.app') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "http://{}:{}/".format(FQDN, PORT)
+URL = "https://bottom-katherina-rjfilestoreha-a892eb74.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://bottom-katherina-rjfilestoreha-a892eb74.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
